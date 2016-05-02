37acbc16d636bffa0702a962cb889ee4f6b3dba3
Basix <jsrtyu1120@gmail.com>
2016-05-02 19:14:40 +0900

    링크 문제 수정

--- module-internal/namumark.js
+++ module-internal/namumark.js
@@ -9,17 +9,11 @@ module.exports = function(n, ba){
   six = six.replace(/<(.*) on(.*)="(.*)">/g, "")
   d('1: '+six)
 
-  // 개행 담당
-  six = six.replace(/<br>/g, "")
-  six = six.replace(/\n\n|\r\n\r\n/g, "<br>")
-  six = six.replace(/\[br\]/g, "<br>")
-  d('2: '+six)
-
   // 앞 태그
   six = six.replace(/>\s([^\n]*)/g, "<blockquote>$1</blockquote>")
   six = six.replace(/##([^#\n]*)/g, "")
   six = six.replace(/#redirect\s(.*)/g, "<div class=\"flash\">[[$1]] 문서를 찾고 계신가요?</div>")
-  d('3: '+six)
+  d('2: '+six)
 
   // 감싸는 태그
   six = six.replace(/-{4,11}/g, "<hr>") // 수평선
@@ -29,7 +23,7 @@ module.exports = function(n, ba){
   six = six.replace(/--([^-]*)--|~~([^~]*)~~/g, "<del>$1</del>") // '''취소선'''
   six = six.replace(/\^\^([^\^]*)\^\^/g, "<sup>$1</sup>") // 위첨자
   six = six.replace(/\,\,([^\,]*)\,\,/g, "<sub>$1</sub>") // 아래첨자
-  d('4: '+six)
+  d('3: '+six)
 
   // 제목들
   six = six.replace(/======\s?([^=]*)\s?======/g, "<h6>$1</h6>")
@@ -37,13 +31,13 @@ module.exports = function(n, ba){
   six = six.replace(/====\s?([^=]*)\s?====/g, "<h4>$1</h4>")
   six = six.replace(/===\s?([^=]*)\s?===/g, "<h3>$1</h3>")
   six = six.replace(/==\s?([^=]*)\s?==/g, "<h2>$1</h2>")
-  d('5: '+six)
+  d('4: '+six)
 
   // 고급 태그
   six = six.replace(/\[\[(https?:\/\/[^\n가-힣ㄱ-ㅎ]*[^\n]*[^\[\]]*)\|([^\[\]]*)]]/g, "<a href=\"$1\">$2</a>") // 커스텀 이름의 다른 곳 링크
   six = six.replace(/\[\[(https?:\/\/[^\n가-힣ㄱ-ㅎ]*[^\n]*[^[\[\]]*)]]/g, "<a href=\"$1\">$1</a>") // 다른 곳 링크
-  six = six.replace(/\[\[([^\[\]]*)\|([^\[\]]*)]]/g, "<a href=\"/w/$1\">$2</a>") // 커스텀 이름의 링크
-  six = six.replace(/\[\[([^[\[\]]*)]]/g, "<a href=\"/w/$1\">$1</a>") // 링크
+  six = six.replace(/\[\[(((?!\[\[).)*)\|(((?!\[\[).)*)]]/g, "<a href=\"/w/$1\">$2</a>") // 커스텀 이름의 링크
+  six = six.replace(/\[\[(((?!\[\[).)*)\]\]/g, "<a href=\"/w/$1\">$1</a>") // 링크
   six = six.replace(/([^\n]*\.(jpeg|jpg|gif|png))/g, "<img src=\"$1\">") // 이미지
   six = six.replace(/\{{{\|\s?([^\{\}\|]*)\s?\|}}}/g, "<table class=\"wiki-closure\"><tbody><tr><td><div class=\"wiki-indent border\">$1<\/div><\/td><\/tr><\/tbody><\/table>")
 
@@ -61,14 +55,14 @@ module.exports = function(n, ba){
 
   six = six.replace(/\{{{(((?!{{{).)*)}}}/g, "<code>$1</code>") // 코드로 바꾸기만 지원
   six = six.replace(/<math>(((?!<math>).)*)<\/math>/g, "<img src=\"https:\/\/latex.codecogs.com/gif.latex?$1\" title=\"$1\" />")
-  d('6: '+six)
+  d('5: '+six)
 
   // 리스트
   six = six.replace(/\s\*\s?([^\n]*)/g, "<li>$1</li>")
 
   six = six.replace(/(([1-9]\.\s?(.*)\n?)+)/g, "<ol>$1<ol>")
   six = six.replace(/(([1-9]\.\s?(.*)\n?)+)/g, "<li>$1<li>")
-  d('7: '+six)
+  d('6: '+six)
 
   // 매크로
   six = six.replace(/\[include\((.*)\)]/g, wiki.include["$1"]) // 틀
@@ -80,6 +74,12 @@ module.exports = function(n, ba){
   six = six.replace(/\[youtube\(([^,]*),\s?height=(.*)\)]/g, "<iframe height=\"$3\" src=\"https:\/\/www.youtube.com\/embed\/$1\" frameborder=\"0\" allowfullscreen><\/iframe>")
 
   six = six.replace(/\[anchor\(([^\[\]]*)\)\]/g, "<div id=\"$1\"></div>")
+  d('7: '+six)
+
+  // 개행 담당
+  six = six.replace(/<br>/g, "")
+  six = six.replace(/\n\n|\r\n\r\n/g, "<br>")
+  six = six.replace(/\[br\]/g, "<br>")
   d('8: '+six)
 
 
