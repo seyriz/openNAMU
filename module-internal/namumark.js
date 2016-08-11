7b52bbae26f90948b47e73015fbf8420fa608795
2DU <min08101@naver.com>
2016-08-11 23:40:51 +0900

    테스트용

--- module-internal/namumark.js
+++ module-internal/namumark.js
@@ -35,9 +35,57 @@ module.exports = function(n, ba){
   
   six = six.replace(/\[\[(https?:\/\/)([^\]\]]*)\|([^\]\]]*)\]\]/g, "<a class=\"out_link\" href=\"$1$2\"><span class=\"contect\">外</span>$3</a>")
   six = six.replace(/\[\[(https?:\/\/)([^\]\]]*)\]\]/g, "<a class=\"out_link\" href=\"$1$2\"><span class=\"contect\">外</span>$1$2</a>")
-  six = six.replace(/\[\[([^\]\]]*)\|([^\]\]]*)\]\]/g, "<a href=\"/w/$1\">$2</a>")
-  six = six.replace(/\[\[([^\]\]]*)\]\]/g, "<a href=\"/w/$1\">$1</a>")
   
+  var test = "/<body>No Content</body>/";
+  var cheerio = require('cheerio');
+  var request = require('request');
+  var yellow = "/\[\[([^\]\]]*)\|([^\]\]]*)\]\]/";
+  var green = "/\[\[([^\]\]]*)\]\]/";
+  var i = 0
+  
+  while(i>10)
+  {
+	if(six.indexOf(yellow) != -1)
+	{	
+		test2 = six.match(yellow);
+		var url = '/raw/' + test2[1];
+		request(url, function(error, response, html){
+		if (error) {throw error};
+		var blue = html;
+		});
+		
+		if(blue.indexOf(test) != -1)
+		{
+			six = six.replace(/\[\[([^\]\]]*)\|([^\]\]]*)\]\]/, "<a class=\"not_thing\" href=\"/w/$1\">$2</a>")
+		}
+		else
+		{
+			six = six.replace(/\[\[([^\]\]]*)\|([^\]\]]*)\]\]/, "<a href=\"/w/$1\">$2</a>")
+		}
+	}
+	else if(six.indexOf(green) != -1)
+	{	
+		test2 = six.match(green);
+		var url = '/raw/' + test2[1];
+		request(url, function(error, response, html){
+		if (error) {throw error};
+		var blue = html;
+		});
+		
+		if(blue.indexOf(test) != -1)
+		{
+			six = six.replace(/\[\[([^\]\]]*)\]\]/, "<a class=\"not_thing\" href=\"/w/$1\">$1</a>")
+		}
+		else
+		{
+			six = six.replace(/\[\[([^\]\]]*)\]\]/, "<a href=\"/w/$1\">$1</a>")
+		}
+	}
+	else
+	{
+		i = 11
+	}
+  }
   six = six.replace(/======\s(.*)\s======[ ]*/g, "<h6>$1</h6>")
   six = six.replace(/=====\s(.*)\s=====[ ]*/g, "<h5>$1</h5>")
   six = six.replace(/====\s(.*)\s====[ ]*/g, "<h4>$1</h4>")
