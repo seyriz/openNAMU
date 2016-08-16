ed3b73abeedeae3d7d0e1a10882acb4750e870a8
2DU <min08101@naver.com>
2016-08-16 23:16:57 +0900

    하루카나 말 들을껄 그랬나

--- module-internal/namumark.js
+++ module-internal/namumark.js
@@ -1,30 +1,30 @@
-var wiki = require('../wiki')
+var wiki = require('../wiki');
 module.exports = function(n, ba){
-  var six = n
-  var today = getNow()
-  var d = require('debug')('openNAMU:parser')
+  var six = n;
+  var today = getNow();
+  var d = require('debug')('openNAMU:parser');
   var docnames = Object.keys(wiki.doc);
 
   function getNow() {
-  var today = new Date()
+  var today = new Date();
   var dd = today.getDate();
   var mm = today.getMonth()+1; 
   var yyyy = today.getFullYear();
   if(dd<10) {
-      dd='0'+dd
+      dd='0'+dd;
   }
   if(mm<10) {
-      mm='0'+mm
+      mm='0'+mm;
   }
   return yyyy+'/' + mm+'/'+dd;
   }
   
-  six = six.replace(/<script>|<\/script>/g, "")
-  six = six.replace(/<(.*) on(.*)="(.*)">/g, "")
-  six = six.replace(/javascript:/g, "")
+  six = six.replace(/<script>|<\/script>/g, "");
+  six = six.replace(/<(.*) on(.*)="(.*)">/g, "");
+  six = six.replace(/javascript:/g, "");
   
-  six = six.replace(/^#redirect ([^\n]*)/g, "<head><meta http-equiv=\"refresh\" content=\"3;url=/w/$1\" /></head><li>3초 후 [[$1]] 문서로 리다이렉트 합니다.</li>")
-  six = six.replace(/^#넘겨주기 ([^\n]*)/g, "<head><meta http-equiv=\"refresh\" content=\"3;url=/w/$1\" /></head><li>3초 후 [[$1]] 문서로 리다이렉트 합니다.</li>")
+  six = six.replace(/^#redirect ([^\n]*)/g, "<head><meta http-equiv=\"refresh\" content=\"3;url=/w/$1\" /></head><li>3초 후 [[$1]] 문서로 리다이렉트 합니다.</li>");
+  six = six.replace(/^#넘겨주기 ([^\n]*)/g, "<head><meta http-equiv=\"refresh\" content=\"3;url=/w/$1\" /></head><li>3초 후 [[$1]] 문서로 리다이렉트 합니다.</li>");
   
   var ohhhh = /\n>\s?(([^\n]*)((\n*(.*))+))/;
   var read
@@ -32,8 +32,8 @@ module.exports = function(n, ba){
   {
 	  if(read = ohhhh.exec(six))
 	  {
-		read[1] = read[1].replace(/\n>\s?/, "\n")
-		six = six.replace(/\n>\s?(([^\n]*)((\n*(.*))+))/, "<blockquote>" + read[1] + "</blockquote>")
+		read[1] = read[1].replace(/\n>\s?/, "\n");
+		six = six.replace(/\n>\s?(([^\n]*)((\n*(.*))+))/, "<blockquote>" + read[1] + "</blockquote>");
 	  }
 	  else
 	  {
@@ -42,18 +42,18 @@ module.exports = function(n, ba){
   }
   
   
-  six = six.replace(/##\s?([^\n]*)/g, "<!--$1-->")
+  six = six.replace(/##\s?([^\n]*)/g, "<!--$1-->");
   
-  six = six.replace(/\[\[분류:([^\]\]]*)\]\]/g, "")
-  six = six.replace(/\[목차\]/g, "")
+  six = six.replace(/\[\[분류:([^\]\]]*)\]\]/g, "");
+  six = six.replace(/\[목차\]/g, "");
   
-  six = six.replace(/\[\[((https?:\/\/)([^\]\]]*)\.(png|jpg|gif|jpeg))\|(.*)\]\]/g, "<a class=\"out_link\" href=\"$1asdf\"><span class=\"contect\">外</span>$5</a>")
-  six = six.replace(/\[\[((https?:\/\/)([^\]\]]*)\.(png|jpg|gif|jpeg))\]\]/g, "<a class=\"out_link\" href=\"$1asdf\"><span class=\"contect\">外</span>$1asdf</a>")
+  six = six.replace(/\[\[((https?:\/\/)([^\]\]]*)\.(png|jpg|gif|jpeg))\|(.*)\]\]/g, "<a class=\"out_link\" href=\"$1asdf\"><span class=\"contect\">外</span>$5</a>");
+  six = six.replace(/\[\[((https?:\/\/)([^\]\]]*)\.(png|jpg|gif|jpeg))\]\]/g, "<a class=\"out_link\" href=\"$1asdf\"><span class=\"contect\">外</span>$1asdf</a>");
   
   var docLinkPattern_Labeled = /\[\[([^\]\]]*)\|([^\]\]]*)\]\]/g,
     docLinkPattern = /\[\[([^\]\]]*)\]\]/g;
-  six = six.replace(/\[\[(https?:\/\/)([^\]\]]*)\|([^\]\]]*)\]\]/g, "<a class=\"out_link\" href=\"$1$2\"><span class=\"contect\">外</span>$3</a>")
-  six = six.replace(/\[\[(https?:\/\/)([^\]\]]*)\]\]/g, "<a class=\"out_link\" href=\"$1$2\"><span class=\"contect\">外</span>$1$2</a>")
+  six = six.replace(/\[\[(https?:\/\/)([^\]\]]*)\|([^\]\]]*)\]\]/g, "<a class=\"out_link\" href=\"$1$2\"><span class=\"contect\">外</span>$3</a>");
+  six = six.replace(/\[\[(https?:\/\/)([^\]\]]*)\]\]/g, "<a class=\"out_link\" href=\"$1$2\"><span class=\"contect\">外</span>$1$2</a>");
   while(true) {
     var match = docLinkPattern_Labeled.exec(six);
     if(match == null)
@@ -79,63 +79,63 @@ module.exports = function(n, ba){
     }
   }
 
-  six = six.replace(/======\s(.*)\s======[ ]*/g, "<h6>$1</h6>")
-  six = six.replace(/=====\s(.*)\s=====[ ]*/g, "<h5>$1</h5>")
-  six = six.replace(/====\s(.*)\s====[ ]*/g, "<h4>$1</h4>")
-  six = six.replace(/===\s(.*)\s===[ ]*/g, "<h3>$1</h3>")
-  six = six.replace(/==\s(.*)\s==[ ]*/g, "<h2>$1</h2>")
-  six = six.replace(/=\s(.*)\s=\n/g, "<h1>$1</h1>\n")
+  six = six.replace(/======\s(.*)\s======[ ]*/g, "<h6>$1</h6>");
+  six = six.replace(/=====\s(.*)\s=====[ ]*/g, "<h5>$1</h5>");
+  six = six.replace(/====\s(.*)\s====[ ]*/g, "<h4>$1</h4>");
+  six = six.replace(/===\s(.*)\s===[ ]*/g, "<h3>$1</h3>");
+  six = six.replace(/==\s(.*)\s==[ ]*/g, "<h2>$1</h2>");
+  six = six.replace(/=\s(.*)\s=\n/g, "<h1>$1</h1>\n");
   
-  six = six.replace(/<!--\s?([^--]*)\s?-->/g, "<not_del>$1</not_del>")
+  six = six.replace(/<!--\s?([^--]*)\s?-->/g, "<not_del>$1</not_del>");
   
-  six = six.replace(/'''(.+?)'''(?!')/g,'<strong>$1</strong>')
-  six = six.replace(/''(.+?)''(?!')/g,'<i>$1</i>')
-  six = six.replace(/~~(.+?)~~(?!~)/g,'<s>$1</s>')
-  six = six.replace(/--(.+?)--(?!-)/g,'<s>$1</s>')
-  six = six.replace(/__(.+?)__(?!_)/g,'<u>$1</u>')
-  six = six.replace(/\^\^(.+?)\^\^(?!\^)/g,'<sup>$1</sup>')
-  six = six.replace(/,,(.+?),,(?!,)/g,'<sub>$1</sub>')
+  six = six.replace(/'''(.+?)'''(?!')/g,'<strong>$1</strong>');
+  six = six.replace(/''(.+?)''(?!')/g,'<i>$1</i>');
+  six = six.replace(/~~(.+?)~~(?!~)/g,'<s>$1</s>');
+  six = six.replace(/--(.+?)--(?!-)/g,'<s>$1</s>');
+  six = six.replace(/__(.+?)__(?!_)/g,'<u>$1</u>');
+  six = six.replace(/\^\^(.+?)\^\^(?!\^)/g,'<sup>$1</sup>');
+  six = six.replace(/,,(.+?),,(?!,)/g,'<sub>$1</sub>');
   
-  six = six.replace(/{{{\+([1-5])\s?((([^}}}]*)*(\n)?)+)}}}(?!})/g,'<span class="font-size-$1">$2</span>')
-  six = six.replace(/{{{\-([1-5])\s?((([^}}}]*)*(\n)?)+)}}}(?!})/g,'<span class="font-size-small-$1">$2</span>')
+  six = six.replace(/{{{\+([1-5])\s?((([^}}}]*)*(\n)?)+)}}}(?!})/g,'<span class="font-size-$1">$2</span>');
+  six = six.replace(/{{{\-([1-5])\s?((([^}}}]*)*(\n)?)+)}}}(?!})/g,'<span class="font-size-small-$1">$2</span>');
   
-  six = six.replace(/{{{(#[0-9a-f-A-F]{3}) +(.*?)}}}(?!})/g,'<span style="color:$1">$2</span>')
-  six = six.replace(/{{{(#[0-9a-f-A-F]{6}) +(.*?)}}}(?!})/g,'<span style="color:$1">$2</span>')
-  six = six.replace(/{{{#(\w+) +(.*?)}}}(?!})/g,'<span style="color:$1">$2</span>')
+  six = six.replace(/{{{(#[0-9a-f-A-F]{3}) +(.*?)}}}(?!})/g,'<span style="color:$1">$2</span>');
+  six = six.replace(/{{{(#[0-9a-f-A-F]{6}) +(.*?)}}}(?!})/g,'<span style="color:$1">$2</span>');
+  six = six.replace(/{{{#(\w+) +(.*?)}}}(?!})/g,'<span style="color:$1">$2</span>');
   
-  six = six.replace(/\[br\]/ig,'<br>')
+  six = six.replace(/\[br\]/ig,'<br>');
   
-  six = six.replace(/{{\|([^\|}}]*)\|}}/g, "<table><tbody><tr><td>$1</td></tr></tbody></table>")
+  six = six.replace(/{{\|([^\|}}]*)\|}}/g, "<table><tbody><tr><td>$1</td></tr></tbody></table>");
   
-  six = six.replace(/\.pngasdf/g, ".peg")
-  six = six.replace(/\.gifasdf/g, ".gef")
-  six = six.replace(/\.jpgasdf/g, ".jeg")
-  six = six.replace(/\.jpegasdf/g, ".jepg")
+  six = six.replace(/\.pngasdf/g, ".peg");
+  six = six.replace(/\.gifasdf/g, ".gef");
+  six = six.replace(/\.jpgasdf/g, ".jeg");
+  six = six.replace(/\.jpegasdf/g, ".jepg");
   
-  six = six.replace(/([^\n]*\.(jpeg|jpg|gif|png))\?width=([^\n]*)&height=([^\n]*)/g, "<img src=\"$1\" width=\"$3\"  height=\"$4\">")
-  six = six.replace(/([^\n]*\.(jpeg|jpg|gif|png))\?height=([^\n]*)&width=([^\n]*)/g, "<img src=\"$1\" width=\"$3\" height=\"$4\">")
-  six = six.replace(/([^\n]*\.(jpeg|jpg|gif|png))\?height=([^\n]*)/g, "<img src=\"$1\" height=\"$3\">")
-  six = six.replace(/([^\n]*\.(jpeg|jpg|gif|png))\?width=([^\n]*)/g, "<img src=\"$1\" width=\"$3\">")
-  six = six.replace(/([^\n]*\.(jpeg|jpg|gif|png))/g, "<img src=\"$1\">")
-  six = six.replace(/<img src="<img src="([^\n]*\.(jpeg|jpg|gif|png))">" width="([^\n]*)" height="([^\n]*)">/g, "<img src=\"$1\" width=\"$3\" height=\"$4\">")
-  six = six.replace(/<img src="<img src="([^\n]*\.(jpeg|jpg|gif|png))">" width="([^\n]*)">/g, "<img src=\"$1\" width=\"$3\">")
-  six = six.replace(/<img src="<img src="([^\n]*\.(jpeg|jpg|gif|png))">" height="([^\n]*)">/g, "<img src=\"$1\" height=\"$3\">")
+  six = six.replace(/([^\n]*\.(jpeg|jpg|gif|png))\?width=([^\n]*)&height=([^\n]*)/g, "<img src=\"$1\" width=\"$3\"  height=\"$4\">");
+  six = six.replace(/([^\n]*\.(jpeg|jpg|gif|png))\?height=([^\n]*)&width=([^\n]*)/g, "<img src=\"$1\" width=\"$3\" height=\"$4\">");
+  six = six.replace(/([^\n]*\.(jpeg|jpg|gif|png))\?height=([^\n]*)/g, "<img src=\"$1\" height=\"$3\">");
+  six = six.replace(/([^\n]*\.(jpeg|jpg|gif|png))\?width=([^\n]*)/g, "<img src=\"$1\" width=\"$3\">");
+  six = six.replace(/([^\n]*\.(jpeg|jpg|gif|png))/g, "<img src=\"$1\">");
+  six = six.replace(/<img src="<img src="([^\n]*\.(jpeg|jpg|gif|png))">" width="([^\n]*)" height="([^\n]*)">/g, "<img src=\"$1\" width=\"$3\" height=\"$4\">");
+  six = six.replace(/<img src="<img src="([^\n]*\.(jpeg|jpg|gif|png))">" width="([^\n]*)">/g, "<img src=\"$1\" width=\"$3\">");
+  six = six.replace(/<img src="<img src="([^\n]*\.(jpeg|jpg|gif|png))">" height="([^\n]*)">/g, "<img src=\"$1\" height=\"$3\">");
   
-  six = six.replace(/\[youtube\(([^,]*),\s?width=(.*),\s?height=(.*)\)]/g, "<iframe width=\"$2\" height=\"$3\" src=\"https:\/\/www.youtube.com\/embed\/$1\" frameborder=\"0\" allowfullscreen><\/iframe>")
-  six = six.replace(/\[youtube\(([^,]*),\s?height=(.*),\s?width=(.*)\)]/g, "<iframe width=\"$3\" height=\"$2\" src=\"https:\/\/www.youtube.com\/embed\/$1\" frameborder=\"0\" allowfullscreen><\/iframe>")
-  six = six.replace(/\[youtube\(([^,]*),\s?width=(.*)\)]/g, "<iframe width=\"$2\" src=\"https:\/\/www.youtube.com\/embed\/$1\" frameborder=\"0\" allowfullscreen><\/iframe>")
-  six = six.replace(/\[youtube\(([^,]*),\s?height=(.*)\)]/g, "<iframe height=\"$3\" src=\"https:\/\/www.youtube.com\/embed\/$1\" frameborder=\"0\" allowfullscreen><\/iframe>")
-  six = six.replace(/\[youtube\((.*)\)]/g, "<iframe width=\"640px\" height=\"360px\" src=\"https://www.youtube.com/embed/$1\" frameborder=\"0\" allowfullscreen></iframe>")
+  six = six.replace(/\[youtube\(([^,]*),\s?width=(.*),\s?height=(.*)\)]/g, "<iframe width=\"$2\" height=\"$3\" src=\"https:\/\/www.youtube.com\/embed\/$1\" frameborder=\"0\" allowfullscreen><\/iframe>");
+  six = six.replace(/\[youtube\(([^,]*),\s?height=(.*),\s?width=(.*)\)]/g, "<iframe width=\"$3\" height=\"$2\" src=\"https:\/\/www.youtube.com\/embed\/$1\" frameborder=\"0\" allowfullscreen><\/iframe>");
+  six = six.replace(/\[youtube\(([^,]*),\s?width=(.*)\)]/g, "<iframe width=\"$2\" src=\"https:\/\/www.youtube.com\/embed\/$1\" frameborder=\"0\" allowfullscreen><\/iframe>");
+  six = six.replace(/\[youtube\(([^,]*),\s?height=(.*)\)]/g, "<iframe height=\"$3\" src=\"https:\/\/www.youtube.com\/embed\/$1\" frameborder=\"0\" allowfullscreen><\/iframe>");
+  six = six.replace(/\[youtube\((.*)\)]/g, "<iframe width=\"640px\" height=\"360px\" src=\"https://www.youtube.com/embed/$1\" frameborder=\"0\" allowfullscreen></iframe>");
   
-  six = six.replace(/\[date]/g, today)
-  six = six.replace(/\[datetime]/g, today)
+  six = six.replace(/\[date]/g, today);
+  six = six.replace(/\[datetime]/g, today);
   
-  six = six.replace(/\[anchor\(([^\[\]]*)\)\]/g, "<div id=\"$1\"></div>")
+  six = six.replace(/\[anchor\(([^\[\]]*)\)\]/g, "<div id=\"$1\"></div>");
   
-  six = six.replace(/\s\*\s([^\n]*)\n/g, "<li>$1</li><not_br></not_br>")
-  six = six.replace(/\s\*\s([^\n]*)/g, "<li>$1</li>")
+  six = six.replace(/\s\*\s([^\n]*)\n/g, "<li>$1</li><not_br></not_br>");
+  six = six.replace(/\s\*\s([^\n]*)/g, "<li>$1</li>");
   
-  six = six.replace(/-{4,11}/g, "<hr>")
+  six = six.replace(/-{4,11}/g, "<hr>");
   
   var a = 1;
   var b = /\[\*\s((?:[^\[\]]+)*)\]/;
@@ -166,12 +166,12 @@ module.exports = function(n, ba){
   six = six.replace(/\.jeg/g, ".jpg")
   six = six.replace(/\.jepg/g, ".jpeg")
   
-  six = six.replace(/\|\|((\s?)*(([^||]*)*(\|\|)*(\s?))*)\|\|((((\n\|\|)*((\s?)*(([^||]*)*(\|\|)*(\s?))*))+)\|\|)?/g, "<table><tbody><tr><td>$1</td></tr></tbody></table>")
-  six = six.replace(/\|\|\r\n\|\|/g, "</td></tr><not_br></not_br><tr><td>")
-  six = six.replace(/\|\|/g, "</td><td>")
+  six = six.replace(/\|\|(<table\s?((width|height)=([^>]*))>)?(<table\s?((width|height)=([^>]*))>)?((\s?)*(([^||]*)*(\|\|)*(\s?))*)\|\|((((\n\|\|)*((\s?)*(([^||]*)*(\|\|)*(\s?))*))+)\|\|)?/g, '<table $2 $6><tbody><tr><td>$9</td></tr></tbody></table>');
+  six = six.replace(/\|\|\r\n\|\|/g, "</td></tr><not_br></not_br><tr><td>");
+  six = six.replace(/\|\|/g, "</td><td>");
   
-  six = six.replace(/\n/g, "<br>")
-  six = six.replace(/<not_br><\/not_br>/g, "\n")
+  six = six.replace(/\n/g, "<br>");
+  six = six.replace(/<not_br><\/not_br>/g, "\n");
   
   six = six.replace(/\[각주\](((<br>+)*(\s+)*(\n+))+)?$/g, "");
   six = six.replace(/\[각주\]/g, "<br>" + tou);
