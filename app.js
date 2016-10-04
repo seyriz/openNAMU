486e16b997fd1d1bac17dc8d2c7703ce06668097
2DU <min08101@naver.com>
2016-10-04 17:30:21 +0900

    없는건 대문으로 가게 함

--- app.js
+++ app.js
@@ -26,9 +26,7 @@ app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  res.redirect('/w/');
 });
 
 
