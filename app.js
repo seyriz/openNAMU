91625fa176c7916e961bd1a957649614eab63f28
2DU <min08101@naver.com>
2016-10-19 18:52:01 +0900

    용량 박살내기

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('view engine', 'pug');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 // app.use(logger('dev')); 로그 제거
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({limit: '10240mb', extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
