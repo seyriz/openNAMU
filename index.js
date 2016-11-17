17e629591ebcafb415e78cc55e386033ffa1c733
2DU <min08101@naver.com>
2016-11-17 23:02:09 +0900

    한번 더

--- index.js
+++ index.js
@@ -1552,7 +1552,7 @@ router.get('/w/:page', function(req, res) {
 				res.end();
 			}
 			else {
-				var redirect = /^\r\n#(?:넘겨주기|redirect)\s([^\n]*)/ig;
+				var redirect = /^#(?:넘겨주기|redirect)\s([^\n]*)/ig;
 				var dtest;
 				if(dtest = redirect.exec(data)) {
 					data = data.replace(redirect, "<head><meta http-equiv=\"refresh\" content=\"0;url=/w/"+encodeURIComponent(dtest[1])+"/redirect/"+encodeURIComponent(req.params.page)+"\" /></head><li>리다이렉트 <a href='$1'>$1</a></li>");
