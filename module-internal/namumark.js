8eaf8fe74131cac673eafd4b4bd7600e9d85d447
2DU <min08101@naver.com>
2016-11-21 19:07:11 +0900

    html 수정

--- module-internal/namumark.js
+++ module-internal/namumark.js
@@ -25,7 +25,7 @@ module.exports = function(req, n, ba){
   six = '\r\n' + six + '\r\n';
   
   six = six.replace(/<((?:\w+)|(?:#[0-9a-f-A-F]{3})|(?:#[0-9a-f-A-F]{6}))>/ig, '[bgcolor=$1]');
-  six = six.replace(/<((?:div|span|font|iframe|table\s?bordercolor=(?:\w+)|table\s?bordercolor=(?:#[0-9a-f-A-F]{3})|table\s?bordercolor=(?:#[0-9a-f-A-F]{6})|table\s?width=(?:[^>]*)|table\s?align=(?:[^>]*)|table\s?bgcolor=(?:\w+)|table\s?bgcolor=(?:#[0-9a-f-A-F]{3})|table\s?bgcolor=(?:#[0-9a-f-A-F]{6})|\:|\(|\)|bgcolor=(?:\w+)|bgcolor=(?:#[0-9a-f-A-F]{3})|bgcolor=(?:#[0-9a-f-A-F]{6})|-(?:[0-9]+)|\|(?:[0-9]+)|\/(?:div|span|font|iframe))(\s[^>]+)?)>/ig, '[$1]');
+  six = six.replace(/<((?:span|font|iframe|table\s?bordercolor=(?:\w+)|table\s?bordercolor=(?:#[0-9a-f-A-F]{3})|table\s?bordercolor=(?:#[0-9a-f-A-F]{6})|table\s?width=(?:[^>]*)|table\s?align=(?:[^>]*)|table\s?bgcolor=(?:\w+)|table\s?bgcolor=(?:#[0-9a-f-A-F]{3})|table\s?bgcolor=(?:#[0-9a-f-A-F]{6})|\:|\(|\)|bgcolor=(?:\w+)|bgcolor=(?:#[0-9a-f-A-F]{3})|bgcolor=(?:#[0-9a-f-A-F]{6})|-(?:[0-9]+)|\|(?:[0-9]+)|big|small|del|s|\/(?:span|font|iframe|big|small|del|s))(\s[^>]+)?)>/ig, '[$1]');
   
   six = xssFilters.inHTMLData(six);
   
@@ -586,7 +586,7 @@ module.exports = function(req, n, ba){
   six = six + tou;
   d('1: '+six)
   
-  six = six.replace(/\[((?:div|span|font|iframe|\/(?:div|span|font|iframe))(\s[^\]]+)?)]/ig, '<$1>');
+  six = six.replace(/\[((?:span|font|iframe|big|small|del|s|\/(?:span|font|iframe|big|small|del|s))(\s[^\]]+)?)]/ig, '<$1>');
   
   //  six = plugin(six);
   ba(six)
